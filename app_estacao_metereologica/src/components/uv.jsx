061294891f753283b2a4f7cb3d61faf9bdd6ae50
ViniciusPolo <admviniciuspolo@gmail.com>
2022-10-05 20:23:02 -0300

    Estruturação React e inclusão Flutter

---
+++ app_estacao_metereologica/src/components/uv.jsx
@@ -0,0 +1,6 @@
+export default function uv(){
+    return(
+        <>
+        </>
+    )
+}
---
+++ app_flutter/android/app/src/main/kotlin/com/example/estacaometereologica/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.estacaometereologica
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
